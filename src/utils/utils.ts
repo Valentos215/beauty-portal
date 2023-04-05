4a318b6416839ff03fbfaf82c6393e016dd05685
Valentos215 <teskavale46@gmail.com>
2023-04-05 21:50:30 +0300

    Add moment to string converters

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,44 +6,46 @@ import moment, { Moment } from 'moment';
 const getStrTime = (time: Moment): string => {
   return `${time.hour()}:${time.minute()}`;
 };
-
-const getMomentTime = (time: string) => {
+const getMomentTime = (time: string): Moment => {
   return moment(time, 'h:m');
+};
+const getStrDate = (date: Moment): string => {
+  return `${date.date()}.${date.month()}`;
+};
+
+const getMomentDate = (date: string): Moment => {
+  return moment(date, 'D.M');
 };
 
 const getArtistDayStatus = (date: Moment, cabinetData: IArtistCabinetData): string => {
   const { weekend, recordAhead, workingHours, breakHours, dateNow, proceduresList } = cabinetData;
   let dayStatus = '';
   if (
-    date < moment(dateNow, 'D.M').add(1, 'month') ||
-    date > moment(dateNow, 'D.M').add(1, 'month').add(recordAhead, 'weeks') ||
+    date < getMomentDate(dateNow).add(1, 'month') ||
+    date > getMomentDate(dateNow).add(1, 'month').add(recordAhead, 'weeks') ||
     weekend.includes(date.day())
   ) {
     dayStatus = EDateStatus.NonWorking;
-  } else if (
-    !proceduresList.some((procedure) => procedure.date === `${date.date()}.${date.month()}`)
-  ) {
+  } else if (!proceduresList.some((procedure) => procedure.date === getStrDate(date))) {
     dayStatus = EDateStatus.Free;
   } else {
-    let currentTime = moment(workingHours[0], 'h:m');
-    let endOfDay = moment(workingHours[1], 'h:m');
+    let currentTime = getMomentTime(workingHours[0]);
+    let endOfDay = getMomentTime(workingHours[1]);
     let freeHoursCounter = 0;
     const proceduresToday = proceduresList.filter(
-      (procedure) => procedure.date === `${date.date()}.${date.month()}`,
+      (procedure) => procedure.date === getStrDate(date),
     );
     while (currentTime < endOfDay) {
       // skip breaks
-      if (`${currentTime.hour()}:${currentTime.minute()}` === breakHours[0]) {
-        currentTime = moment(breakHours[1], 'h:m');
+      if (getStrTime(currentTime) === breakHours[0]) {
+        currentTime = getMomentTime(breakHours[1]);
       }
-      let currentTimeString = `${currentTime.hour()}:${currentTime.minute()}`;
+      let currentTimeString = getStrTime(currentTime);
       let currentProcedure = proceduresToday.find((proc) => proc.startTime === currentTimeString);
       // skip procedure time
       if (currentProcedure) {
-        currentTime = moment(currentProcedure.startTime, 'h:m').add(
-          currentProcedure.duration,
-          'minutes',
-        );
+        const { startTime, duration } = currentProcedure;
+        currentTime = getMomentTime(startTime).add(duration, 'minutes');
         // add 30 minutes
       } else {
         freeHoursCounter++;
@@ -63,8 +65,8 @@ export const createArtistCalendar = (artistCabinetData: IArtistCabinetData): IDa
   const months: IDayWithStatus[][] = [];
   let chunk: IDayWithStatus[] = [];
   const { recordAhead, dateNow } = artistCabinetData;
-  const currentDate = moment(dateNow, 'D.M').add(1, 'month').startOf('week').add(1, 'day');
-  const endOfLastMonth = moment(dateNow, 'D.M')
+  const currentDate = getMomentDate(dateNow).add(1, 'month').startOf('week').add(1, 'day');
+  const endOfLastMonth = getMomentDate(dateNow)
     .add(1, 'month')
     .add(recordAhead, 'weeks')
     .endOf('month');
@@ -73,7 +75,7 @@ export const createArtistCalendar = (artistCabinetData: IArtistCabinetData): IDa
     // first month
     if (!chunk.length || chunk[0].date.split('.')[1] === String(currentDate.month())) {
       chunk.push({
-        date: `${currentDate.date()}.${currentDate.month()}`,
+        date: getStrDate(currentDate),
         status: getArtistDayStatus(currentDate, artistCabinetData),
       });
       // moving on to the next month
@@ -91,7 +93,7 @@ export const createArtistCalendar = (artistCabinetData: IArtistCabinetData): IDa
         });
       }
       chunk.push({
-        date: `${currentDate.date()}.${currentDate.month()}`,
+        date: getStrDate(currentDate),
         status: getArtistDayStatus(currentDate, artistCabinetData),
       });
     }
@@ -111,17 +113,15 @@ export const createClientCalendar = (clientCabinetData: IClientCabinetData): IDa
   const { proceduresList, dateNow } = clientCabinetData;
   const months: IDayWithStatus[][] = [];
   let chunk: IDayWithStatus[] = [];
-  const currentDate = moment(dateNow, 'D.M').add(1, 'month').startOf('week').add(1, 'day');
-  let endOfMonth = moment(dateNow, 'D.M').add(1, 'month').endOf('month');
-  let endOfWeek = moment(dateNow, 'D.M').add(1, 'month').endOf('week').add(1, 'day');
+  const currentDate = getMomentDate(dateNow).add(1, 'month').startOf('week').add(1, 'day');
+  let endOfMonth = getMomentDate(dateNow).add(1, 'month').endOf('month');
+  let endOfWeek = getMomentDate(dateNow).add(1, 'month').endOf('week').add(1, 'day');
   const shift = 7 - endOfWeek.diff(endOfMonth, 'days');
   const endOfNextMonth = endOfMonth.clone().add(1, 'day').endOf('month');
   const getStatus = (date: Moment) => {
-    if (date < moment(dateNow, 'D.M')) {
+    if (date < getMomentDate(dateNow)) {
       return EDateStatus.NonWorking;
-    } else if (
-      proceduresList.some((procedure) => procedure.date === `${date.date()}.${date.month()}`)
-    ) {
+    } else if (proceduresList.some((procedure) => procedure.date === getStrDate(date))) {
       return EDateStatus.PartFilled;
     } else {
       return EDateStatus.Free;
@@ -131,7 +131,7 @@ export const createClientCalendar = (clientCabinetData: IClientCabinetData): IDa
     // first month
     if (!chunk.length || chunk[0].date.split('.')[1] === String(currentDate.month())) {
       chunk.push({
-        date: `${currentDate.date()}.${currentDate.month()}`,
+        date: getStrDate(currentDate),
         status: getStatus(currentDate),
       });
       // moving on to the next month
@@ -146,7 +146,7 @@ export const createClientCalendar = (clientCabinetData: IClientCabinetData): IDa
         });
       }
       chunk.push({
-        date: `${currentDate.date()}.${currentDate.month()}`,
+        date: getStrDate(currentDate),
         status: getStatus(currentDate),
       });
     }
@@ -173,10 +173,10 @@ export const createDayTimeline = (
   const { workingHours, breakHours, proceduresList } = artistCabinetData;
   let timeLine: ITimeLine;
   let dayTimeline = [];
-  let currentTime = moment(workingHours[0], 'h:m');
-  let endOfDay = moment(workingHours[1], 'h:m');
+  let currentTime = getMomentTime(workingHours[0]);
+  let endOfDay = getMomentTime(workingHours[1]);
   while (currentTime < endOfDay) {
-    const currentTimeString = `${currentTime.hour()}:${currentTime.minute()}`;
+    const currentTimeString = getStrTime(currentTime);
     if (currentTimeString === breakHours[0]) {
       dayTimeline.push({
         startTime: breakHours[0],
@@ -184,16 +184,16 @@ export const createDayTimeline = (
         categoryTitle: ETimeTitle.BreakHours,
         timeStatus: ETimeStatus.BreakHours,
       });
-      currentTime = moment(breakHours[1], 'h:m');
+      currentTime = getMomentTime(breakHours[1]);
       continue;
     }
     const currentProcedure = proceduresList.find((proc) => proc.startTime === currentTimeString);
     if (currentProcedure) {
       const { startTime, duration } = currentProcedure;
-      const endOfProcedure = moment(startTime, 'h:m').add(duration, 'minutes');
+      const endOfProcedure = getMomentTime(startTime).add(duration, 'minutes');
       dayTimeline.push({
-        startTime: currentProcedure.startTime,
-        endTime: `${endOfProcedure.hour()}:${endOfProcedure.minute()}`,
+        startTime: startTime,
+        endTime: getStrTime(endOfProcedure),
         categoryTitle: ETimeTitle.BreakHours,
         timeStatus: ETimeStatus.BreakHours,
       });
